eedcc6d8d196697f400134a340039df5d59622f7
demamano <demaamano@gmail.com>
2022-08-26 17:20:52 +0300

    necessary tools installation and environment setup

--- ADBLOGS/blog/models.py
+++ ADBLOGS/blog/models.py
@@ -4,11 +4,14 @@ class Reporter(models.Model):
     def __str__(self):
         return self.full_name
 class Article(models.Model):
-    pub_date = models.DateField()
+    pub_date = models.DateField(auto_now_add=True)
     headline = models.CharField(max_length=200)
     content = models.TextField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.headline
-    
+    # from blog.models import Article,Reporter
+    # Reporter.objects.get(id=1)
+    # Article.objects.create(headline='In pictures: Russia invades Ukraine',content="Russia had been tightening its military grip around Ukraine since last year, amassing tens of thousands of soldiers, as well as equipment and artillery, on its neighbor's doorstep. The escalation in the years-long conflict has triggered the greatest security crisis in Europe since the Cold War, and it has also sparked an intense showdown between Western powers and Moscow.Editor's note: This gallery contains graphic images. Viewer discretion is advised.",r)
+
--- ADBLOGS/blog/views.py
+++ ADBLOGS/blog/views.py
@@ -1,2 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Article,Reporter
+from .serializers import ArticleSerializer
+@api_view(['GET'])
+def getData(request):
+    items = Article.objects.all()
+    serializer = ArticleSerializer(items,many=True)
+    return Response(serializer.data)
+
+
+
 
